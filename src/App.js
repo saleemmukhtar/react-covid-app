93bf5eea0df1a8158eec33f9bf8b91e65706a632
saleemmukhtar <75995330+saleemmukhtar@users.noreply.github.com>
2021-07-06 17:34:08 +0500

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+import Covid from './Covid';
+function App() {
+  return (
+   <div>
+     <Header />
+<Covid />
+
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+function Header(){
+    return(
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#"><img src='https://www.zurich.com/it-it/products-and-services/-/media/project/zurich/dotcom/pictograms/covid-19.png' width='120px' /></a>
+            
+            
+            
+        </div>
+        <h1 Style='margin-left:-80px'>Covid-19 Results</h1>
+    </nav>
+
+    )
+}
+
+export default Header
